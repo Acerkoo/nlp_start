8e986ea5dc84a2810767f5395866a16bd2551c62
Acerkoo <36981220+Acerkoo@users.noreply.github.com>
2020-12-22 21:09:01 +0800

    rnn

--- d2l_pytorch.py
+++ d2l_pytorch.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import torchvision
 from IPython import display
@@ -40,4 +42,58 @@ def load_data_fashion_mnist(batch_size):
 
     return train_iter, test_iter
 
-# def
+def load_data_jay_lyrics():
+    with open('./Data/jaychou_lyrics.txt', 'r', encoding='utf-8') as fin:
+        corpus_chars = fin.read()
+
+    corpus_chars = corpus_chars.replace('\r', ' ').replace('\n', ' ')
+    corpus_chars = corpus_chars[:10000]
+    idx_to_char = list(set(corpus_chars))
+    char_to_idx = dict([(chr, i) for i, chr in enumerate(idx_to_char)])
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    # 减1是因为输出的索引x是相应输入的索引y加1
+    num_examples = (int(len(corpus_indices)) - 1) // num_steps
+    epoch_size = num_examples // batch_size
+    example_indices = list(range(num_examples))
+    random.shuffle(example_indices)
+
+    # 返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * batch_size
+        batch_indices = example_indices[i: i + batch_size]
+        X = [_data(j * num_steps) for j in batch_indices]
+        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        yield torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(Y, dtype=torch.float32, device=device)
+
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    corpus_indices = torch.tensor(corpus_indices, dtype=torch.float32, device=device)
+    data_len = int(len(corpus_indices))
+    batch_len = data_len // batch_size
+    indices = corpus_indices[0: batch_size*batch_len].view(batch_size, batch_len)
+    epoch_size = (batch_len - 1) // num_steps
+    for i in range(epoch_size):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y
+
+def one_hot(x, n_class, dtype=torch.float32):
+    x = x.long()
+    res = torch.zeros(x.shape[0], n_class, dtype=dtype, device=x.device)
+    res.scatter_(1, x.view(-1, 1), 1)
+    return res
+
+def to_onehot(X, n_class):
+    return [one_hot(X[:, i], n_class) for i in range(X.shape[1])]
